f12d62e81e8d49056453ca32454891ad58aed4c6
FurqanNoor <82640979+FurqanNoor@users.noreply.github.com>
2025-01-06 14:35:12 +0200

    base

---
+++ src/lib/site-config.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: "Plynt.io",
+  url: "http://plynt.io/",
+  description:
+    "Plynt.io is a habit tracking app that helps you build good habits, break bad ones, and stay motivated through a unique social media twist.",
+  ogImage: "",
+  twitterHandle: "@furggoo",
+  lastUpdated: "December 15, 2024",
+};
